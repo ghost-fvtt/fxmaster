180652fa943d214257b17320c7cc2983474d4d26
o-reo <em.ruaud@gmail.com>
2020-03-11 16:20:02 +0100

    FIX: multiple weather effects generated

--- fxmaster/effects/FXMasterLayer.js
+++ fxmaster/effects/FXMasterLayer.js
@@ -1,122 +1,127 @@
 export class FXMasterLayer extends CanvasLayer {
 
-  constructor() {
-    super();
-    this.effects = {};
-    this.weather = null;
-  }
-
-  /* -------------------------------------------- */
-  /*  Methods
-  /* -------------------------------------------- */
-
-  async draw() {
-    super.draw();
-  }
-
-  /* -------------------------------------------- */
-
-  updateMask() {
-    // Setup scene mask
-    if (this.mask)
-      this.removeChild(this.mask);
-    this.mask = new PIXI.Graphics;
-    this.addChild(this.mask);
-    const d = canvas.dimensions;
-    this.mask.beginFill(0xFFFFFF);
-    if (canvas.background.img) {
-      this.mask.drawRect(d.paddingX - d.shiftX, d.paddingY - d.shiftY, d.sceneWidth, d.sceneHeight);
-    } else {
-      this.mask.drawRect(0, 0, d.width, d.height);
-    }
-  }
-
-  /** @override */
-  tearDown() {
-    this.weather = null;
-    const effKeys = Object.keys(this.effects);
-    for (let i = 0; i < effKeys.length; ++i) {
-      this.effects[effKeys[i]].fx.stop();
-      delete this.effects[effKeys[i]];
-    }
-    return super.tearDown();
-  }
-
-  drawWeather() {
-    if (!this.weather) this.weather = this.addChild(new PIXI.Container());
-    const effKeys = Object.keys(this.effects);
-    for (let i = 0; i < effKeys.length; ++i) {
-      this.effects[effKeys[i]].fx.stop();
+    constructor() {
+        super();
+        this.effects = {};
+        this.weather = null;
     }
 
-    // Updating scene weather
-    const flags = canvas.scene.data.flags.fxmaster;
-    if (flags && flags.effects) {
-      const keys = Object.keys(flags.effects);
-      for (let i = 0; i < keys.length; ++i) {
-        // Effect already exists  
-        if (hasProperty(this.effects, keys[i])) {
-          this.effects[keys[i]].fx.play();
-          continue;
+    /* -------------------------------------------- */
+    /*  Methods
+    /* -------------------------------------------- */
+
+    async draw() {
+        super.draw();
+    }
+
+    /* -------------------------------------------- */
+
+    updateMask() {
+        // Setup scene mask
+        if (this.mask)
+            this.removeChild(this.mask);
+        this.mask = new PIXI.Graphics;
+        this.addChild(this.mask);
+        const d = canvas.dimensions;
+        this.mask.beginFill(0xFFFFFF);
+        if (canvas.background.img) {
+            this.mask.drawRect(d.paddingX - d.shiftX, d.paddingY - d.shiftY, d.sceneWidth, d.sceneHeight);
+        } else {
+            this.mask.drawRect(0, 0, d.width, d.height);
         }
-        this.effects[keys[i]] = {
-          type: flags.effects[keys[i]].type,
-          fx: new CONFIG.weatherEffects[flags.effects[keys[i]].type](this.weather)
-        };
-        this.configureEffect(keys[i]);
-        this.effects[keys[i]].fx.play();
-      };
     }
-  }
 
-  configureEffect(id) {
-    const flags = canvas.scene.data.flags.fxmaster;
-    // Adjust density
-    let factor = 2 * flags.effects[id].config.density / 100;
-    this.effects[id].fx.emitters.forEach(el => {
-      el.frequency *= factor;
-      el.maxParticles *= factor;
-    });
-    // Adjust scale
-    factor = 2 * flags.effects[id].config.scale / 100;
-    this.effects[id].fx.emitters.forEach(el => {
-      el.startScale.value *= factor;
-      let node = el.startScale.next;
-      while (node) {
-        node.value *= factor;
-        node = node.next;
-      }
-    });
-
-    // Adjust speed
-    factor = 2 * flags.effects[id].config.speed / 100;
-    this.effects[id].fx.emitters.forEach(el => {
-      el.startSpeed.value *= factor;
-      let node = el.startSpeed.next;
-      while (node) {
-        node.value *= factor;
-        node = node.next;
-      }
-    });
-
-    // Adjust tint
-    if (flags.effects[id].config.apply_tint) {
-      this.effects[id].fx.emitters.forEach(el => {
-        let colors = hexToRGB(colorStringToHex(flags.effects[id].config.tint));
-        el.startColor.value = { r: colors[0] * 255, g: colors[1] * 255, b: colors[2] * 255 };
-        let node = el.startColor.next;
-        while (node) {
-          node.value = el.startColor.value;
-          node = node.next;
+    /** @override */
+    tearDown() {
+        this.weather = null;
+        const effKeys = Object.keys(this.effects);
+        for (let i = 0; i < effKeys.length; ++i) {
+            this.effects[effKeys[i]].fx.stop();
+            delete this.effects[effKeys[i]];
         }
-      });
+        return super.tearDown();
     }
 
-    // Adjust direction
-    factor = 360 * (flags.effects[id].config.direction - 50) / 100;
-    this.effects[id].fx.emitters.forEach(el => {
-      el.minStartRotation += factor;
-      el.maxStartRotation += factor;
-    });
-  }
+    drawWeather() {
+        if (!this.weather) this.weather = this.addChild(new PIXI.Container());
+        const effKeys = Object.keys(this.effects);
+        for (let i = 0; i < effKeys.length; ++i) {
+            this.effects[effKeys[i]].fx.stop();
+            delete this.effects[effKeys[i]];
+        }
+
+        // Updating scene weather
+        const flags = canvas.scene.data.flags.fxmaster;
+        if (flags && flags.effects) {
+            const keys = Object.keys(flags.effects);
+            for (let i = 0; i < keys.length; ++i) {
+                // Effect already exists
+                if (hasProperty(this.effects, keys[i])) {
+                    this.effects[keys[i]].fx.play();
+                    continue;
+                }
+                this.effects[keys[i]] = {
+                    type: flags.effects[keys[i]].type,
+                    fx: new CONFIG.weatherEffects[flags.effects[keys[i]].type](this.weather)
+                };
+                this.configureEffect(keys[i]);
+                this.effects[keys[i]].fx.play();
+            };
+        }
+    }
+
+    configureEffect(id) {
+        const flags = canvas.scene.data.flags.fxmaster;
+        // Adjust density
+        let factor = 2 * flags.effects[id].config.density / 100;
+        this.effects[id].fx.emitters.forEach(el => {
+            el.frequency *= factor;
+            el.maxParticles *= factor;
+        });
+        // Adjust scale
+        factor = 2 * flags.effects[id].config.scale / 100;
+        this.effects[id].fx.emitters.forEach(el => {
+            el.startScale.value *= factor;
+            let node = el.startScale.next;
+            while (node) {
+                node.value *= factor;
+                node = node.next;
+            }
+        });
+
+        // Adjust speed
+        factor = 2 * flags.effects[id].config.speed / 100;
+        this.effects[id].fx.emitters.forEach(el => {
+            el.startSpeed.value *= factor;
+            let node = el.startSpeed.next;
+            while (node) {
+                node.value *= factor;
+                node = node.next;
+            }
+        });
+
+        // Adjust tint
+        if (flags.effects[id].config.apply_tint) {
+            this.effects[id].fx.emitters.forEach(el => {
+                let colors = hexToRGB(colorStringToHex(flags.effects[id].config.tint));
+                el.startColor.value = {
+                    r: colors[0] * 255,
+                    g: colors[1] * 255,
+                    b: colors[2] * 255
+                };
+                let node = el.startColor.next;
+                while (node) {
+                    node.value = el.startColor.value;
+                    node = node.next;
+                }
+            });
+        }
+
+        // Adjust direction
+        factor = 360 * (flags.effects[id].config.direction - 50) / 100;
+        this.effects[id].fx.emitters.forEach(el => {
+            el.minStartRotation += factor;
+            el.maxStartRotation += factor;
+        });
+    }
 }
--- fxmaster/module/fxmaster.js
+++ fxmaster/module/fxmaster.js
@@ -1,54 +1,75 @@
-import {BubblesWeatherEffect} from "../effects/BubblesWeatherEffect.js";
-import {CloudsWeatherEffect} from "../effects/CloudsWeatherEffect.js";
-import {EmbersWeatherEffect} from "../effects/EmbersWeatherEffect.js";
-import {CrowsWeatherEffect} from "../effects/CrowsWeatherEffect.js";
-import {BatsWeatherEffect} from "../effects/BatsWeatherEffect.js";
-import {FogWeatherEffect} from "../effects/FogWeatherEffect.js";
-import {RaintopWeatherEffect} from "../effects/RaintopWeatherEffect.js";
-import {FXColorFilter} from "../filters/FXColorFilter.js";
-import {FXDizzyFilter} from "../filters/FXDizzyFilter.js";
-import {FXMasterLayer} from "../effects/FXMasterLayer.js";
-import {filterManager} from "../filters/FilterManager.js";
+import {
+    BubblesWeatherEffect
+} from "../effects/BubblesWeatherEffect.js";
+import {
+    CloudsWeatherEffect
+} from "../effects/CloudsWeatherEffect.js";
+import {
+    EmbersWeatherEffect
+} from "../effects/EmbersWeatherEffect.js";
+import {
+    CrowsWeatherEffect
+} from "../effects/CrowsWeatherEffect.js";
+import {
+    BatsWeatherEffect
+} from "../effects/BatsWeatherEffect.js";
+import {
+    FogWeatherEffect
+} from "../effects/FogWeatherEffect.js";
+import {
+    RaintopWeatherEffect
+} from "../effects/RaintopWeatherEffect.js";
+import {
+    FXColorFilter
+} from "../filters/FXColorFilter.js";
+import {
+    FXDizzyFilter
+} from "../filters/FXDizzyFilter.js";
+import {
+    FXMasterLayer
+} from "../effects/FXMasterLayer.js";
+import {
+    filterManager
+} from "../filters/FilterManager.js";
 
 Hooks.once("init", function () {
-  // Adding custom weather effects
-  mergeObject(CONFIG.weatherEffects, {
-    bubbles: BubblesWeatherEffect,
-    clouds: CloudsWeatherEffect,
-    embers: EmbersWeatherEffect,
-    crows: CrowsWeatherEffect,
-    bats: BatsWeatherEffect,
-    fog: FogWeatherEffect,
-    raintop: RaintopWeatherEffect
-  });
+    // Adding custom weather effects
+    mergeObject(CONFIG.weatherEffects, {
+        bubbles: BubblesWeatherEffect,
+        clouds: CloudsWeatherEffect,
+        embers: EmbersWeatherEffect,
+        crows: CrowsWeatherEffect,
+        bats: BatsWeatherEffect,
+        fog: FogWeatherEffect,
+        raintop: RaintopWeatherEffect
+    });
 
-  // Adding filters
-  if (!CONFIG.fxmaster) CONFIG.fxmaster = {};
-  mergeObject(CONFIG.fxmaster, {
-    filters: {
-      dizzy: FXDizzyFilter,
-      color: FXColorFilter
-    }
-  });
+    // Adding filters
+    if (!CONFIG.fxmaster) CONFIG.fxmaster = {};
+    mergeObject(CONFIG.fxmaster, {
+        filters: {
+            dizzy: FXDizzyFilter,
+            color: FXColorFilter
+        }
+    });
 });
 
 Hooks.once('canvasInit', (canvas) => {
-  canvas.fxmaster = canvas.stage.addChildAt(new FXMasterLayer(canvas), 8);
+    canvas.fxmaster = canvas.stage.addChildAt(new FXMasterLayer(canvas), 8);
 });
 
 Hooks.on('canvasInit', canvas => {
-  filterManager.clear();
-  filterManager.switch("color");
+    filterManager.clear();
 });
 
 Hooks.on('canvasReady', (_) => {
-  filterManager.activate();
-  canvas.fxmaster.updateMask();
-  canvas.fxmaster.drawWeather();
+    filterManager.activate();
+    canvas.fxmaster.updateMask();
+    canvas.fxmaster.drawWeather();
 });
 
 Hooks.on("updateScene", (scene, data, options) => {
-  canvas.fxmaster.updateMask();
-  canvas.fxmaster.drawWeather();
-  filterManager.update();
+    canvas.fxmaster.updateMask();
+    canvas.fxmaster.drawWeather();
+    filterManager.update();
 });
