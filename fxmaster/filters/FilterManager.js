3a5d18ecc6c42ce62056a2889ff7423bfaad30f0
mesfoliesludiques <ewolfwood@yandex.com>
2020-03-11 03:53:00 +0100

    FIX: client update

--- fxmaster/filters/FilterManager.js
+++ fxmaster/filters/FilterManager.js
@@ -1,27 +1,31 @@
 class FilterManager {
     constructor() {
-        this.filters = {}
+        this.filterInfos = {};
+        this.filters = {};
     }
 
     activate() {
         this.update();
     }
 
-    addFilter(filter, options) {
-        this.filterInfos[randomID()] = {
-            type: filter,
-            options: options
-        };
-        this.dump();
-    }
-
     update() {
         const flags = canvas.scene.data.flags.fxmaster;
         if (flags && flags.filters) {
             this.filterInfos = flags.filters;
-        } else if (game.user.isGM) {
-            canvas.scene.setFlag("fxmaster", "filters", {});
         }
+
+        // Clear unused effects
+        const effkeys = Object.keys(this.filters);
+        for (let i = 0; i < effkeys.length; ++i) {
+            if (this.filterInfos[effkeys[i]]) {
+                continue;
+            }
+            this.filters[effkeys[i]].stop().then((_, res) => {
+                delete this.filters[effkeys[i]];
+            });
+        }
+
+        // create new effects
         const keys = Object.keys(this.filterInfos);
         for (let i = 0; i < keys.length; ++i) {
             if (this.filters[keys[i]]) {
@@ -29,6 +33,7 @@ class FilterManager {
             }
             this.filters[keys[i]] = new CONFIG.fxmaster.filters[this.filterInfos[keys[i]].type](this.filterInfos[keys[i]].options);
         }
+
         canvas.background.filters = Object.values(this.filters);
         canvas.tiles.filters = Object.values(this.filters);
         canvas.tokens.filters = Object.values(this.filters);
@@ -49,15 +54,23 @@ class FilterManager {
         }
     }
 
+    addFilter(filter, options) {
+        this.filterInfos[randomID()] = {
+            type: filter,
+            options: options
+        };
+        this.dump();
+    }
+
     switch(filter) {
         const keys = Object.keys(this.filters);
         for (let i = 0; i < keys.length; ++i) {
-            console.log(this.filters[keys[i]]);
             if (this.filterInfos[keys[i]].type == filter) {
                 delete this.filterInfos[keys[i]];
                 this.filters[keys[i]].stop().then((_, res) => {
                     delete this.filters[keys[i]];
                 });
+                this.dump();
                 return;
             }
         }
